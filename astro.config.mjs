cc0e21ade37c71766f295becbb722edf4502e0b4
Lyan Ketin Kanon Runningwind Roffi Roffe <roffi.peng@foxmail.com>
2024-10-17 21:34:41 +0800

    u

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,2 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
-
-export default defineConfig({
-  integrations: [react()],
-
-  output: "server",
-  adapter: cloudflare()
-});

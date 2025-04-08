0e1835dce4bc09969408628f5b8c770f620fe885
Lyan Ketin Kanon Runningwind Roffi Roffe <x@pengxianzhe.org>
2025-04-08 15:59:21 +0800

    i

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -16,8 +16,24 @@ export default function CardsContainer(props) {
         })
         .flatMap(item => item.content)
         .sort((a, b) => {
-          return a.title < b.title ? -1 : 1;
+          const isChinese = text => /[\u4e00-\u9fa5]/.test(text.charAt(0));
+        
+          if (!isChinese(a.title) && isChinese(b.title)) {
+            // 如果 a 是英文，而 b 是中文，a 排在前面
+            return -1;
+          }
+          if (isChinese(a.title) && !isChinese(b.title)) {
+            // 如果 a 是中文，而 b 是英文，a 排在后面
+            return 1;
+          }
+        
+          // 否则都为中文或都为英文时，按拼音顺序排序
+          return a.title.localeCompare(b.title, 'zh-Hans-CN', { sensitivity: 'variant' });
         })
+        // .sort((a, b) => a.title.localeCompare(b.title, 'zh-Hans-CN', { sensitivity: 'variant' }))
+        // .sort((a, b) => {
+        //   return a.title < b.title ? -1 : 1;
+        // })
         .map(({url, title, body, tag}, i) => {
           return <Card
             key={i}
